a23b3fe9cfd52a56c07db2604f270c466331363b
LBR474 <LB572179@gmail.com>
2022-10-17 09:26:56 +1100

    Monday morning close down commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,11 @@ import { NgtGLTFLoader } from '@angular-three/soba/loaders';
 })
 export class AppComponent implements OnInit {
   title = 'Angular-TKD_4';
-  public color = new THREE.Color('white');
+  public color = new THREE.Color('pink');
+  multi: string[][] = [
+    ['#b3478c', '#1e62c0', '#ffa764', '#3de68b', '#a11f2a', '#ffbf00'],
+    ['Pink', 'Blue', 'Orange', 'Green', 'Red', 'Yellow'],
+  ];
 
   colors: string[] = [
     '#b3478c',
@@ -25,7 +29,7 @@ export class AppComponent implements OnInit {
     '#ffbf00',
   ];
 
-  selectedColor = this.colors[0]
+  selectedColor = this.colors[0];
 
   model$ = this.ngtGLTFLoader.load('/assets/TKD_girl_4.gltf');
 
--- src/app/xyz/xyz.component.ts
+++ src/app/xyz/xyz.component.ts
@@ -9,6 +9,7 @@ import { gsap } from 'gsap';
 
 import {
   Bone,
+  Euler,
   Mesh,
   MeshStandardMaterial,
   Scene,
@@ -23,13 +24,36 @@ import { Object3D } from 'three';
 import { NgtGLTFLoader } from '@angular-three/soba/loaders';
 import { NgtSkeletonHelper } from '@angular-three/core/helpers';
 
+// // component decorator area begins
+
 @Component({
   selector: 'app-xyz',
   templateUrl: './xyz.component.html',
   styleUrls: ['./xyz.component.css'],
 })
+
+// // xyz class area begins
 export class XyzComponent implements OnInit {
+  //
+  // // class-wide scoped variable area begins
   public colorfirst = new THREE.Color('aqua');
+  public color = new THREE.Color('gray');
+
+  model$ = this.ngtGLTFLoader.load('/assets/TKD_girl_4.gltf');
+
+  modelMaterial: MeshStandardMaterial | undefined;
+
+  public ninety_degrees: number = Math['PI'] / 2;
+
+  public boneNames: string[] = [];
+
+  public spine: Object3D | undefined;
+  public shoulderL: Object3D | undefined;
+  public shoulderR: Object3D | undefined;
+  public pelvisL: Object3D | undefined;
+  public pelvisR: Object3D | undefined;
+
+  // // input area begins
 
   @Input()
   set colorer(value: string) {
@@ -38,44 +62,22 @@ export class XyzComponent implements OnInit {
   }
   #color = '';
   title = 'Angular-TKD_4';
-  public color = new THREE.Color('white');
-  public rotation: number = Math['PI'] / 2;
-
-  public boneNames: string[] = [];
-
-  model$ = this.ngtGLTFLoader.load('/assets/TKD_girl_4.gltf');
-
-  modelMaterial: MeshStandardMaterial | undefined;
-  public modelBone: Object3D | undefined;
 
   constructor(private ngtGLTFLoader: NgtGLTFLoader) {}
 
   ngOnInit(): void {}
 
-  applyColor(colorer: string) {
-    if (this.modelMaterial) {
-      this.modelMaterial.color.setHex(parseInt(colorer.substring(1), 16));
-    }
-    //this.modelBone?.rotateZ(this.rotation);
-    if (this.modelBone) {
-      gsap.to(this.modelBone.rotation, {
-        duration: 1,
-        y: '+=1.57',
-      });
-    }
-  }
-
+  //
+  // // modelready area begins
   modelReady(object: Object3D) {
     const wholeGirl = object.name;
     const mesh = object.getObjectByName('metarig');
 
-    this.modelBone =
-      object.getObjectByName(
-        'metarig'
-      )?.children[0].children[0].children[0].children[0].children[1];
-    if (this.modelBone) {
-      this.boneNames.push(this.modelBone?.name);
-    }
+    this.spine = object.getObjectByName('spine');
+    this.shoulderL = object.getObjectByName('upper_armL');
+    this.shoulderR = object.getObjectByName('upper_armR');
+    this.pelvisL = object.getObjectByName('pelvisL');
+    this.pelvisR = object.getObjectByName('pelvisR');
 
     this.modelMaterial = <MeshStandardMaterial>(
       (<Mesh>object.getObjectByName('Sphere001_4')).material
@@ -83,7 +85,49 @@ export class XyzComponent implements OnInit {
     //this.modelMaterial.wireframe = true;
     //this.modelMaterial.color.setHex(parseInt('6c238c', 16))
     this.applyColor(this.#color);
-    //console.log(mesh)
-    console.log(this.boneNames);
+    // console.log(mesh);
+    //console.log(this.boneNames);
+  }
+
+  attention = () => {
+    if (
+      this.shoulderL &&
+      this.spine &&
+      this.shoulderR &&
+      this.pelvisL &&
+      this.pelvisR
+    ) {
+      //  gsap.to(this.spine.rotation, {
+      //    duration: 1,
+      //    z: '+=0.785',
+      //  });
+      gsap.to(this.shoulderL.rotation, {
+        duration: 1,
+        z: '-=0.69 ',
+      });
+
+      gsap.to(this.shoulderR.rotation, {
+        duration: 1,
+        z: '+=0.69',
+      });
+      gsap.to(this.pelvisL.rotation, {
+        duration: 1,
+        z: '-=0.2',
+      });
+      gsap.to(this.pelvisR.rotation, {
+        duration: 1,
+        z: '+=0.2',
+      });
+      gsap.to(this.spine.position, {
+        duration: 1,
+        x: '-=0.1',
+      });
+    }
+  };
+
+  applyColor(colorer: string) {
+    if (this.modelMaterial) {
+      this.modelMaterial.color.setHex(parseInt(colorer.substring(1), 16));
+    }
   }
 }
