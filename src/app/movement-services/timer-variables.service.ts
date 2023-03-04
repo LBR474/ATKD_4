d2cb7cb42dde0d31aa8d7e286b491b8631bc7654
LockB <109570062+LBR474@users.noreply.github.com>
2023-03-04 13:42:39 +1100

    Update timer-variables.service.ts

--- src/app/movement-services/timer-variables.service.ts
+++ src/app/movement-services/timer-variables.service.ts
@@ -7,7 +7,7 @@ export class TimerVariablesService {
   up_arms_timer = 0.45;
   down_arms_timer = 0.15;
   camera_z_pos = 1.2;
-  min_distance = 1.4;
+  min_distance = 1.0;
 
   full_turn_duration = this.up_arms_timer + this.down_arms_timer;
 
