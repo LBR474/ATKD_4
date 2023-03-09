0240faa7be6cbd7b6108bd02f558024ee244a94e
Fake Name <fakename@fake.com>
2023-03-10 05:49:55 +1100

    First down home commit

--- src/myFirstTestSpec.js
+++
@@ -1,9 +0,0 @@
-describe("Protractor Testing", function () {
-  it("to check the page title", function () {
-    browser.ignoreSynchronization = true;
-    browser.get("http://localhost:4200/");
-    browser.driver.getTitle().then(function (pageTitle) {
-      expect(pageTitle).toEqual("TKD basic poomsae game");
-    });
-  });
-});
